d40e554f6433f27bb39197a8fec88a1ef883b3bf
akoya-tomo <akoya_tomo-00755@yahoo.co.jp>
2019-04-26 06:01:43 +0900

    ログサイトを追加

--- koshian_reload_futaba/options.js
+++ koshian_reload_futaba/options.js
@@ -6,8 +6,9 @@ const DEFAULT_REPLACE_F5_KEY = false;
 const DEFAULT_REFRESH_DELETED_RES = false;
 const DEFAULT_REFRESH_SOUDANE = false;
 const DEFAULT_REFRESH_IDIP = false;
-//const DEFAULT_USE_FUTABACHIN_LINK = false;
 const DEFAULT_USE_FUTAPO_LINK = false;
+const DEFAULT_USE_FTBUCKET_LINK = false;
+const DEFAULT_USE_TSUMANNE_LINK = false;
 const DEFAULT_SCROLL_TO_TOP = false;
 let scroll_period = null;
 let count_to_reload = null;
@@ -17,8 +18,9 @@ let replace_f5_key = null;
 let refresh_deleted_res = null;
 let refresh_soudane = null;
 let refresh_idip = null;
-//let use_futabachin_link = null;
 let use_futapo_link = null;
+let use_ftbucket_link = null;
+let use_tsumanne_link = null;
 let scroll_to_top = null;
 
 function onError(error) {
@@ -38,8 +40,9 @@ function saveSetting(e) {
         refresh_deleted_res:refresh_deleted_res.checked,
         refresh_soudane:refresh_soudane.checked,
         refresh_idip:refresh_idip.checked,
-        //use_futabachin_link:use_futabachin_link.checked,
         use_futapo_link:use_futapo_link.checked,
+        use_ftbucket_link:use_ftbucket_link.checked,
+        use_tsumanne_link:use_tsumanne_link.checked,
         scroll_to_top:scroll_to_top.checked
     });
 }
@@ -53,8 +56,9 @@ function setCurrentChoice(result) {
     refresh_deleted_res.checked = safeGetValue(result.refresh_deleted_res, DEFAULT_REFRESH_DELETED_RES);    
     refresh_soudane.checked = safeGetValue(result.refresh_soudane, DEFAULT_REFRESH_SOUDANE);    
     refresh_idip.checked = safeGetValue(result.refresh_idip, DEFAULT_REFRESH_IDIP);    
-    //use_futabachin_link.checked = safeGetValue(result.use_futabachin_link, DEFAULT_USE_FUTABACHIN_LINK);    
     use_futapo_link.checked = safeGetValue(result.use_futapo_link, DEFAULT_USE_FUTAPO_LINK);    
+    use_ftbucket_link.checked = safeGetValue(result.use_ftbucket_link, DEFAULT_USE_FTBUCKET_LINK);
+    use_tsumanne_link.checked = safeGetValue(result.use_tsumanne_link, DEFAULT_USE_TSUMANNE_LINK);
     scroll_to_top.checked = safeGetValue(result.scroll_to_top, DEFAULT_SCROLL_TO_TOP);    
 }
 
@@ -67,8 +71,9 @@ function onLoad() {
     refresh_deleted_res = document.getElementById("refresh_deleted_res");
     refresh_soudane = document.getElementById("refresh_soudane");
     refresh_idip = document.getElementById("refresh_idip");
-    //use_futabachin_link = document.getElementById("use_futabachin_link");
     use_futapo_link = document.getElementById("use_futapo_link");
+    use_ftbucket_link = document.getElementById("use_ftbucket_link");
+    use_tsumanne_link = document.getElementById("use_tsumanne_link");
     scroll_to_top = document.getElementById("scroll_to_top");
 
     scroll_period.addEventListener("change", saveSetting);
@@ -79,8 +84,9 @@ function onLoad() {
     refresh_deleted_res.addEventListener("change", saveSetting);
     refresh_soudane.addEventListener("change", saveSetting);
     refresh_idip.addEventListener("change", saveSetting);
-    //use_futabachin_link.addEventListener("change", saveSetting);
     use_futapo_link.addEventListener("change", saveSetting);
+    use_ftbucket_link.addEventListener("change", saveSetting);
+    use_tsumanne_link.addEventListener("change", saveSetting);
     scroll_to_top.addEventListener("change", saveSetting);
 
     browser.storage.local.get().then(setCurrentChoice, onError);
